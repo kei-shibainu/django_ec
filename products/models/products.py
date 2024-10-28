7d4ee8c985960b977440bb234bcb0e848b3afdaf
kei-shibainu <hotaru.personal.works@gmail.com>
2024-10-29 04:37:19 +0900

    商品登録、一覧、削除ページ追加

--- products/models/products.py
+++ products/models/products.py
@@ -3,32 +3,20 @@ import uuid
 
 def file_upload_path(instance, filename):
     return f'uploads/{instance.id}/{filename}'
-
-class Category(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=100)
-
-    def __str__(self) -> str:
-        return self.name
-    
-    class Meta:
-        db_table = 'Categories'
-
 # Create your models here.
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=100)
-    description = models.TextField(null=True, blank=True)
-    price = models.PositiveIntegerField(default=0)
-    stock = models.PositiveIntegerField(default=0)
-    sold_quantity = models.PositiveIntegerField(default=0)
-    review = models.PositiveIntegerField(default=0)
-    discount_rate = models.PositiveIntegerField(default=0)
-    discount_price = models.PositiveIntegerField(default=0, editable=False)
-    image = models.ImageField(default='', upload_to=file_upload_path, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='categories')
+    name = models.CharField(max_length=100, verbose_name="商品名")
+    description = models.TextField(null=True, blank=True, verbose_name="説明")
+    price = models.PositiveIntegerField(default=0, verbose_name="価格")
+    stock = models.PositiveIntegerField(default=0, verbose_name="在庫数")
+    sold_quantity = models.PositiveIntegerField(default=0, editable=False, verbose_name="販売数")
+    review = models.PositiveIntegerField(default=0, editable=False, verbose_name="レビュー")
+    discount_rate = models.PositiveIntegerField(default=0, verbose_name="割引率")
+    discount_price = models.PositiveIntegerField(default=0, editable=False, verbose_name="割引率")
+    image = models.ImageField(default='', upload_to=file_upload_path, blank=True, verbose_name="画像")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="作成日")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="更新日")
 
     def save(self, *args, **kwargs):
         if 0 < self.discount_rate:
@@ -42,4 +30,4 @@ class Product(models.Model):
         return self.name
     
     class Meta:
-        db_table = 'Products'
+        db_table = 'products'
--- products/views/admin_view.py
+++ products/views/admin_view.py
@@ -1,5 +1,8 @@
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
+from products.forms.admin_forms import ProductForm
 from products.models.products import Product
 from products.views.basic_auth_view import BasicAuthMixIn
 
@@ -12,3 +15,20 @@ class AdminProductsView(BasicAuthMixIn, ListView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'My Book Shop -管理者商品一覧'
         return context
+
+class AdminProductCreateView(BasicAuthMixIn, CreateView):
+    model = Product
+    template_name = "admin/produict_form.html"
+    form_class = ProductForm
+    success_url = reverse_lazy('admin-products')
+
+class AdminProductEditView(UpdateView):
+    model = Product
+    template_name = "admin/produict_form.html"
+    form_class = ProductForm
+    success_url = reverse_lazy('admin-products')
+
+class AdminProductDeleteView(DeleteView):
+    model = Product
+    template_name = "admin/produict_confirm_delete.html"
+    success_url = reverse_lazy('admin-products')
